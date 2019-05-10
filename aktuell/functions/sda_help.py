4f724914b79f5fbfc8544272da9734ffb52fa0b3
GFlach <flach@htw-dresden.de>
2019-05-10 20:08:25 +0200

    stand 19-05-10

--- aktuell/functions/sda_help.py
+++ aktuell/functions/sda_help.py
@@ -6,6 +6,7 @@ Hilfsfunktionen für Datenanalyse (TSV)
 @author: gudrun
 """
 
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
@@ -315,3 +316,33 @@ def perceptron(X, y, w, alpha, n_iter):
         if err == 0:
             break
     return(i, w)    
+
+def plot_decision_regions(X, y, w, resolution=0.1):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    #Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = np.dot(w[1:,],np.array([xx1.ravel(), xx2.ravel()]))
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
